1923446d587c9391e7d4eed7fd2fbf2d2ec3daf5
Lucas Rivoal <lucas.rivoal@etu.u-bordeaux.fr>
2026-03-19 15:23:37 +0100

    ✨ Ajout d'un composent tache plus demarrage d'une liste de taches simple

--- frontend/task-manager/src/App.jsx
+++ frontend/task-manager/src/App.jsx
@@ -1,15 +1,45 @@
-import { useEffect } from 'react'
-
+import { useEffect, useState } from 'react'
+import Task from "./Task.jsx"
 import './App.css'
 
 function App() {
+  const [tasks, setTasks] = useState(["Faire apparaitre des tâches", "Transformer le map","Faire en sorte d'ajouter un bouton",]); // Liste des tâches
+  const [input, setInput] = useState("");
   useEffect(() =>{
     fetch("http://localhost:3000")
     .then(res => res.text())
     .then(data => console.log(data))
   }, []);
 
-  return <h1>Frontend Running</h1>
+  const addTask = () => {
+    if(!input.trim()){
+      return;
+    }else{
+      setTasks([...tasks,input]);
+      setInput("");
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title_temporary">Frontend Running</h1>
+      <h2 className="subtitle-tasks-list">Liste des tâches</h2>
+      <div className="list-task">
+        <ul>
+          {tasks.map((task,index) =>(
+            <Task
+              title={task} 
+              key={index}
+            />
+          ))}
+        </ul>
+      </div>
+      <div className="menu_barre">
+        <input type="text" className="new_task" value={input} onChange={(e) => setInput(e.target.value)} />
+        <button className="button_new_task" type="button" onClick={addTask}> Ajouter </button>
+      </div>
+    </div>
+  )
 
   
 }
---
+++ frontend/task-manager/src/Task.jsx
@@ -0,0 +1,9 @@
+function Task({title}){
+    return(
+        <li className="task_element">
+            {title}
+        </li>
+    )
+}
+
+export default Task;
